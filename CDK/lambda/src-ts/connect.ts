3a623d221e89eb9ce4a87193264785874f58eba6
Patrick Henry <patrick.henry@humanit.us>
2021-09-21 16:22:10 -0400

    finished adding the documentation and final code to the lambda

--- CDK/lambda/src-ts/connect.ts
+++ CDK/lambda/src-ts/connect.ts
@@ -1,14 +1,196 @@
-import { 
-  APIGatewayProxyEvent, 
-  APIGatewayProxyResult 
+import {
+  ConnectContactFlowEvent,
+  ConnectContactFlowResult,
+  ConnectContactFlowCallback,
+  Context,
 } from "aws-lambda";
+import * as AWS from "aws-sdk";
+const words = require("an-array-of-english-words");
+
 export const lambdaHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  console.log("event ", event)
-  const queries = JSON.stringify(event.queryStringParameters);
-  return {
-    statusCode: 200,
-    body: `Queries: ${queries}`
+  event: ConnectContactFlowEvent,
+  context: Context,
+  callback: ConnectContactFlowCallback
+): Promise<ConnectContactFlowResult> => {
+  console.log("event ", event);
+  //console.log("wordlist ", wordlist.length);
+
+  AWS.config.update({ region: process.env.region });
+  const dynamoClient = new AWS.DynamoDB.DocumentClient();
+
+  try {
+    var { customerNumber } = event.Details.Parameters; // grab the customerNumber attribute passed through from the contact flow
+    // const words = wordlist.filter(
+    //   (item) => item.length === customerNumber.length
+    // );
+    const validNumber = validateNumber(customerNumber); // use some Regex magic to ensure that it is a valid 10 digit number sans country code
+
+    console.log("processed Number ", validNumber);
+
+    const vanityList = await generateVanityNumbers(
+      validNumber,
+      dynamoClient,
+      words
+    );
+
+    console.log("vanityList ", vanityList);
+
+    const result: ConnectContactFlowResult = {};
+
+    const finalVanityList = vanityList.slice(-3); // taking the last three (or fewer) elements of the array to return to Connect
+
+    for (let i = 0; i < finalVanityList.length; i++) {
+      result["number" + i] = finalVanityList[i].replace(/(.)/g, "$&, ");
+    }
+
+    return result;
+  } catch (err) {
+    throw new Error(err);
   }
-}
+};
+
+/**
+ * validates phone number
+ * @param phone
+ */
+const validateNumber = (number: string): string => {
+  // This expression matches valid, ten digit US phone numbers
+  const validPhoneNumber = /^(\+1|1)?(\d{10})$/;
+
+  if (!number) {
+    throw Error("Phone number was null or undefined.");
+  }
+
+  if (!number.match(validPhoneNumber)) {
+    throw Error("Invalid phone number.");
+  }
+
+  return number.replace(validPhoneNumber, "$2");
+};
+
+const generateVanityNumbers = async (
+  number: string,
+  dynamoClient: AWS.DynamoDB.DocumentClient,
+  words: string[]
+): Promise<string[]> => {
+  let vanityList: string[] = await checkNumber(number, dynamoClient);
+  if (vanityList) {
+    //if the vanityList is already in the database, return it
+    return vanityList;
+  }
+
+  vanityList = [];
+
+  const dialPadMap = new Map([
+    ["0", "0"],
+    ["1", "1"],
+    ["2", "ABC"],
+    ["3", "DEF"],
+    ["4", "GHI"],
+    ["5", "JKL"],
+    ["6", "MNO"],
+    ["7", "PQRS"],
+    ["8", "TUV"],
+    ["9", "WXYZ"],
+  ]);
+
+  const firstSix: string = number.slice(0, 6);
+  const lastFour = number.slice(6).split("");
+
+  const spotOneStr = dialPadMap.get(lastFour[0]).split("");
+  const spotTwoStr = dialPadMap.get(lastFour[1]).split("");
+  const spotThreeStr = dialPadMap.get(lastFour[2]).split("");
+  const spotFourStr = dialPadMap.get(lastFour[3]).split("");
+
+  //create a map data structure that maps integer phone pad digits to their alphabetical counterparts
+
+  for (let i = 0; i < spotOneStr.length; i++) {
+    if (vanityList.length >= 10) {
+      // list already contains 5 words
+      break;
+    }
+
+    for (let j = 0; j < spotTwoStr.length; j++) {
+      if (vanityList.length >= 10) {
+        // list already contains 5 words
+        break;
+      }
+
+      for (let k = 0; k < spotThreeStr.length; k++) {
+        if (vanityList.length >= 10) {
+          // list already contains 5 words
+          break;
+        }
+
+        for (let m = 0; m < spotFourStr.length; m++) {
+          if (vanityList.length >= 10) {
+            // list already contains 5 words
+            break;
+          }
+
+          const phoneWord =
+            spotOneStr[i] + spotTwoStr[j] + spotThreeStr[k] + spotFourStr[m];
+          const vanityNumber = firstSix + phoneWord;
+          if (vanityList.length < 5) {
+            // take the first 5 permutations (or fewer if lastFour is e.g. 1111)
+            vanityList.push(vanityNumber);
+          } else if (words.includes(phoneWord.toLowerCase())) {
+            // will add up to the first five matches in the dictionary
+            vanityList.push(vanityNumber);
+          }
+        }
+      }
+    }
+  }
+
+  vanityList = vanityList.slice(-5); //only consider the last 5 (or fewer) elements added
+
+  await save(number, vanityList, dynamoClient).catch((error) => {
+    throw new Error(error);
+  });
+
+  return vanityList;
+};
+
+const checkNumber = async (
+  number: string,
+  dynamoClient: AWS.DynamoDB.DocumentClient
+): Promise<string[]> => {
+  const partitionKey = process.env.PARTITION_KEY;
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      [partitionKey]: number,
+    },
+  };
+
+  const result = await dynamoClient.get(params).promise();
+
+  console.log("result ", result);
+
+  try {
+    console.log("Found vanity numbers in db: " + result.Item["vanity_numbers"]);
+    return result.Item["vanity_numbers"];
+  } catch (err) {
+    return null; //returns null if no vanity_numbers attribute exists
+  }
+};
+
+const save = async (
+  number: string,
+  vanityList: string[],
+  dynamoClient: AWS.DynamoDB.DocumentClient
+) => {
+  const partitionKey = process.env.PARTITION_KEY;
+  const params: any = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      [partitionKey]: number,
+      vanity_numbers: vanityList,
+    },
+    ConditionExpression: "attribute_not_exists(phone_number)", // do not overwrite existing entries, but shouldn't trigger since checkNumber() handles this
+    ReturnConsumedCapacity: "TOTAL",
+  };
+
+  await dynamoClient.put(params).promise();
+};
--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -1,20 +1,50 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
-
+import * as cdk from "@aws-cdk/core";
+import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
+import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
+import { AttributeType, Table } from "@aws-cdk/aws-dynamodb";
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaFunction = new lambda.Function(this, 'connected', {
-      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda/built'),  // code loaded from "lambda" directory, then complied to ts from js and sent to the built dir
-      handler: 'connect.lambdaHandler'                // file is "connect", function is "lambdaHandler"
+    const dynamoTable = new Table(this, id, {
+      partitionKey: {
+        name: "phone_number",
+        type: AttributeType.STRING,
+      },
+      tableName: "vanityNumbers",
+
+      /**
+       *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new table, to help with cleanup of this project I have set the removalpolicy to destroy
+       */
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
     });
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: lambdaFunction
+    const lambdaFunction = new Function(this, "connected", {
+      runtime: Runtime.NODEJS_14_X, // execution environment
+      code: Code.fromAsset("lambda/built"), // code loaded from "lambda" directory, then complied to ts from js and sent to the built dir
+      handler: "connect.lambdaHandler", // file is "connect", function is "lambdaHandler"
+      memorySize: 2048,
+      timeout: cdk.Duration.seconds(8),
+      environment: {
+        region: cdk.Stack.of(this).region,
+        PARTITION_KEY: "phone_number",
+        TABLE_NAME: dynamoTable.tableName,
+      }, // env variables that will be referenced in the lambda runtime to access the correct table
     });
 
+    dynamoTable.grantReadWriteData(lambdaFunction); // grant the lambda access to our dynamodb table
+
+    new LambdaRestApi(this, "EndPoint", {
+      //level 3 construct that is an abstraction of common resource use cases, in this case an apigateway that acts as a request proxy for a lambda function.
+      handler: lambdaFunction,
+    });
+
+    const arn = lambdaFunction.functionArn;
+
+    new cdk.CfnOutput(this, "Output", {
+      value: arn,
+      description: "The arn of the lambda function",
+    });
   }
 }
